8d7afe3300660b07a6716995367c259c4eeb3e32
francis <franciskibira017@gmail.com>
2022-09-14 12:14:53 +0300

    done

--- src/components/Buyer.js
+++ src/components/Buyer.js
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
 import React from 'react'
 
 export default function Buyer() {
+
+  const[buyers, setBuyers] = useState([])
+ console.log(buyers)
+
+  const customers=buyers.map(cus=>{
+    return(
+      <tr className="p-2 border-2">
+      <td className="p-2 border-2">{cus.name.toUpperCase()}</td>
+      <td className="p-2 border-2">{cus.gender.toUpperCase()}</td>
+      <td className="p-2 border-2">{cus.contact.toUpperCase()}</td>
+    </tr>
+    )
+  })
+
+
+  useEffect( () => {
+    fetch("https://fashion-and-design-backend.herokuapp.com/buyers")
+    .then(res => res.json())
+    .then(rosemary => setBuyers(rosemary))
+  },[])
   return (
-    <div>Buyer</div>
+    <div>
+    <table>
+  <tr>
+    <th className="p-2 border-2">Name</th>
+    <th className="p-2 border-2">Gender</th>
+    <th className="p-2 border-2">Contacts</th>
+  </tr>
+  {customers}
+
+
+</table>
+    </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,20 @@ import './Home.css'
 
 function Home(){
     const[sellers, setSellers] = useState([])
+    console.log(sellers)
+
+    const squad=sellers.map(squ=>{
+        return(
+            <div>
+            <h1>Name-{squ.name.toUpperCase()}</h1>
+            <p>Good in getting {squ.speciality.toUpperCase()}</p>
+            </div>
+            
+        )
+    })
   
     useEffect( () => {
-        fetch("https://group-3-backend-app.herokuapp.com/courses")
+        fetch("https://fashion-and-design-backend.herokuapp.com/sellers")
         .then(res => res.json())
         .then(rosemary => setSellers(rosemary))
       },[])
@@ -18,25 +29,29 @@ function Home(){
     return(
         <div>
             <section className="">
-                
+            <div className="rowcon flex">
+            <div className="img">
+                    <img className="h-96" src={ndula}/>
+                </div>
+            <div className="content mt-48 ml-12">
+                <span className="text-4xl">FASHION AND DESIGN</span>
+                <h2>Kuja uvae nasi</h2>
+                <p>Don't hesistate just come and get we are </p>ready to dress you
+                <button id="enr"><a href=" " id="enrl">Shop now</a></button>
+                <button id="ex"><a href=" " id="exp">we are there</a></button>
+            </div>  
+                     
+        </div>
             </section>
             <section>
-                <div className="rowcon">
-                    <div className="img">
-                            <img src={ndula}/>
-                        </div>
-                    <div className="content">
-                        <span>fashion and design</span>
-                        <h2>kuja uvae nasi</h2>
-                        <p>don't hesistate just come and get we are </p>ready to dress you
-                        <button id="enr"><a href=" " id="enrl">Shop now</a></button>
-                        <button id="ex"><a href=" " id="exp">we are there</a></button>
-                    </div>  
-                             
-                </div>
-                <div className="rowcon">
-                <div className="img">
-                <img src="https://kitengestore.com/wp-content/uploads/2019/07/mens-red-blue-floral-custom-made-african-print-long-sleeve-shirt-model-wearing-front-view-520x694.jpg" alt=" " />
+                
+                <div className="rowcon flex h-1/2">
+                <div className="img h-96">
+                <img className="h-3/4" src="https://kitengestore.com/wp-content/uploads/2019/07/mens-red-blue-floral-custom-made-african-print-long-sleeve-shirt-model-wearing-front-view-520x694.jpg" alt=" " />
+            </div>
+            <div className="ml-28">
+            <h2 className="text-4xl">Meet MY Sellers</h2>
+            {squad}
             </div>
             </div>
             </section>
